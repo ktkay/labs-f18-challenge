3bbb843e63426a636363751e3471db2a51fe9b4b
ktkay <35470603+ktkay@users.noreply.github.com>
2018-09-01 01:05:44 -0400

    import request and json

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +9,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<string:query>', methods=['GET'])
+def pokemon(query):
+    response = requests.get('http://pokeapi.co/api/v2/pokemon/'+str(query)+'/')
+    return render_template('pokemon.html', response = response.json(), query = query)
 
 if __name__ == '__main__':
     app.run()
